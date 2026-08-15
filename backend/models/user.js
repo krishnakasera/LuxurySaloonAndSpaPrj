71684efab83c2a7aa0bcbeb039eb34bda0896701
KrishnaKasera <kishan8114270058@gmail.com>
2026-08-15 20:45:11 +0530

    Fix User model compilation

--- backend/models/user.js
+++ backend/models/user.js
@@ -39,6 +39,8 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+// Prevent OverwriteModelError on Vercel/serverless
+const User =
+  mongoose.models.User || mongoose.model("User", userSchema);
 
 module.exports = User;
